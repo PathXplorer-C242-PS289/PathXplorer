e2ad9a475b415d2322e8ef134c8bd84a7bbc164f
depuntj <danan12453@gmail.com>
2024-12-03 00:25:31 +0700

    added customview for email dan password

---
+++ app/src/main/java/com/example/pathxplorer/ui/utils/customview/PasswordEditText.kt
@@ -0,0 +1,39 @@
+package com.example.pathxplorer.ui.utils.customview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+
+class PasswordEditText : TextInputEditText {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty() && s.length < 8) {
+                    error = "Password must be at least 8 characters"
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+}
