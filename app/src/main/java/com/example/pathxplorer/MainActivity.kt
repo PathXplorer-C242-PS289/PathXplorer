0fa055049253b52d52a8389533b31704cbdd0a51
depuntj <danan12453@gmail.com>
2024-12-04 19:01:09 +0700

    fix merge error

--- app/src/main/java/com/example/pathxplorer/MainActivity.kt
+++ app/src/main/java/com/example/pathxplorer/MainActivity.kt
@@ -5,30 +5,21 @@ import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.pathxplorer.databinding.ActivityMainBinding
-import com.example.pathxplorer.ui.auth.LoginActivity
 import com.example.pathxplorer.ui.main.MainViewModel
 import com.example.pathxplorer.ui.main.SplashActivity
 import com.example.pathxplorer.ui.utils.UserViewModelFactory
 
-
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel by viewModels<MainViewModel> {
-        UserViewModelFactory.getInstance(this)
-    }
-
     private lateinit var binding: ActivityMainBinding
+
     private val viewModel by viewModels<MainViewModel> {
         UserViewModelFactory.getInstance(this)
     }
@@ -36,48 +27,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         viewModel.getSession().observe(this) { user ->
             if (!user.isLogin) {
                 startActivity(Intent(this, SplashActivity::class.java))
                 finish()
-            }
-        }
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-        viewModel.getSession().observe(this) { user ->
-            if (!user.isLogin) {
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
+                return@observe
             }
         }
 
         val colorActionBar = if (isDarkModeEnabled()) "#121212" else "#FFFFFF"
-        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(colorActionBar)))
-        supportActionBar?.elevation = 0f
-        supportActionBar?.hide()
-        
-//        enableEdgeToEdge()
+        supportActionBar?.apply {
+            setBackgroundDrawable(ColorDrawable(Color.parseColor(colorActionBar)))
+            elevation = 0f
+            hide()
+        }
 
-//        val colorActionBar = if (isDarkModeEnabled()) "#121212" else "#FFFFFF"
-//        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#ffffff")))
-//        supportActionBar?.elevation = 0f
-
-        val navView: BottomNavigationView = binding.navView
+        val navView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_account
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_account
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
-//    private fun isDarkModeEnabled(): Boolean {
-//        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-//        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
-//    }
+    private fun isDarkModeEnabled(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
+    }
 }
