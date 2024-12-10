b0d196e73e90482d601f44ac3f3dec6f1f83a34e
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-12-11 01:16:09 +0700

    fix : get instance repo and add card daily quiz

--- app/src/main/java/com/example/pathxplorer/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/pathxplorer/ui/main/MainViewModel.kt
@@ -3,7 +3,9 @@ package com.example.pathxplorer.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
+import com.example.pathxplorer.data.Result
 import com.example.pathxplorer.data.models.UserModel
 import com.example.pathxplorer.data.UserRepository
 import kotlinx.coroutines.launch
@@ -16,6 +18,15 @@ class MainViewModel(private val repository: UserRepository): ViewModel() {
     fun logout() {
         viewModelScope.launch {
             repository.logout()
+        }
+    }
+
+    fun getTestResults() = liveData {
+        emit(Result.Loading)
+        try {
+            emit(repository.getTestResults().value ?: Result.Error("Failed to load test results"))
+        } catch (e: Exception) {
+            emit(Result.Error(e.message ?: "Unknown error occurred"))
         }
     }
 
