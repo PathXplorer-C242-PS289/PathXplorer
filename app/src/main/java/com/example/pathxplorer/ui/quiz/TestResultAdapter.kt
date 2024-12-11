8dda4b3120783bf98f89fb667e6e51a3443e5d65
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-12-11 11:33:19 +0700

    rebase

--- app/src/main/java/com/example/pathxplorer/ui/quiz/TestResultAdapter.kt
+++ app/src/main/java/com/example/pathxplorer/ui/quiz/TestResultAdapter.kt
@@ -1,44 +1,61 @@
 package com.example.pathxplorer.ui.quiz
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pathxplorer.R
 import com.example.pathxplorer.data.remote.response.TestResultsItem
 import com.example.pathxplorer.databinding.TestsItemBinding
 
-class TestResultAdapter(
-    private val onItemClick: (TestResultsItem) -> Unit
-) : ListAdapter<TestResultsItem, TestResultAdapter.TestViewHolder>(DIFF_CALLBACK) {
-
+class TestResultAdapter: ListAdapter<TestResultsItem, TestResultAdapter.TestViewHolder>(DIFF_CALLBACK) {
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<TestResultsItem>() {
-            override fun areItemsTheSame(oldItem: TestResultsItem, newItem: TestResultsItem): Boolean {
+            override fun areItemsTheSame(
+                oldItem: TestResultsItem,
+                newItem: TestResultsItem
+            ): Boolean {
                 return oldItem.testId == newItem.testId
             }
 
-            override fun areContentsTheSame(oldItem: TestResultsItem, newItem: TestResultsItem): Boolean {
+            override fun areContentsTheSame(
+                oldItem: TestResultsItem,
+                newItem: TestResultsItem
+            ): Boolean {
                 return oldItem == newItem
             }
         }
     }
 
-    inner class TestViewHolder(private val binding: TestsItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(test: TestResultsItem) {
-            binding.testTitle.text = test.riasecType
-            binding.testDescription.text = test.interestDescription
-            binding.careerRecommendation.text = test.exampleCareers
-            binding.tvKeySkills.text = test.keySkills
+    class TestViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private var binding = TestsItemBinding.bind(itemView)
 
-            binding.root.setOnClickListener {
-                onItemClick(test)
+        fun bind(test: TestResultsItem) {
+
+            val imageResource = when(test.riasecType) {
+                "R" -> R.drawable.r_type
+                "I" -> R.drawable.i_type
+                "A" -> R.drawable.a_type
+                "S" -> R.drawable.s_type
+                "E" -> R.drawable.e_type
+                "C" -> R.drawable.c_type
+                else -> R.drawable.r_type
+            }
+
+            with(binding) {
+                testImage.setImageResource(imageResource)
+                testDescription.text = test.interestDescription
+                careerRecommendation.text = test.exampleCareers
+                tvKeySkills.text = test.keySkills
             }
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder {
-        val binding = TestsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = LayoutInflater.from(parent.context).inflate(R.layout.tests_item, parent, false)
         return TestViewHolder(binding)
     }
 
